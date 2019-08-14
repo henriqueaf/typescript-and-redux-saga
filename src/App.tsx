2b9f86418009b621407c82c816eec1b30a0375be
Henrique Almeida <henrique_af1@msn.com>
2019-08-14 12:24:27 -0300

    Configure eslint, prettier and tpescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-const App = () => <div className="App" />
+const App = () => <div className="App"></div>;
 
 export default App;
