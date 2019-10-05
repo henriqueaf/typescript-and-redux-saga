4cc3ce7ad9999d40ecc935462bf34bda5d79eab3
Henrique Almeida <henrique_af1@msn.com>
2019-10-05 18:28:02 -0300

    Create repositories saga's test

---
+++ src/store/ducks/repositories/__tests__/sagas.test.ts
@@ -0,0 +1,47 @@
+import { call, put } from 'redux-saga/effects';
+
+import { load } from '../sagas';
+import * as actions from '../actions';
+import api from '../../../../services/api';
+
+describe('repositories sagas', () => {
+  test('when load success', () => {
+    const generator = load();
+
+    expect(generator.next().value).toEqual(
+      call(api.get, 'users/diego3g/repos')
+    );
+
+    const response = {
+      data: [
+        {
+          id: 2,
+          name: 'repository 2',
+        },
+      ],
+    };
+
+    expect(generator.next(response).value).toEqual(
+      put(actions.loadSuccess(response.data))
+    );
+
+    expect(generator.next().done).toBe(true);
+  });
+
+  test('when load fail', () => {
+    const generator = load();
+
+    expect(generator.next().value).toEqual(
+      call(api.get, 'users/diego3g/repos')
+    );
+
+    if (generator.throw) {
+      const error = new Error('server dead');
+      expect(generator.throw(error).value).toEqual(
+        put(actions.loadFailure(error))
+      );
+    }
+
+    expect(generator.next().done).toBe(true);
+  });
+});
