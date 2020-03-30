f7c8ac9f9c9a89363b7aeb7ec83edcdebae54168
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 14:09:22 +0000

    Deploy henriqueaf/typescript-and-redux-saga to github.com/henriqueaf/typescript-and-redux-saga.git:gh-pages

---
+++ precache-manifest.c9dbd23cd5bee73513fef08d22e70194.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "27316d0bc8d381acf436",
+    "url": "/typescript-and-redux-saga/static/js/main.6435cf9e.chunk.js"
+  },
+  {
+    "revision": "de39e325db9b159b35d6",
+    "url": "/typescript-and-redux-saga/static/js/runtime~main.58198f54.js"
+  },
+  {
+    "revision": "24a52965373914d4363c",
+    "url": "/typescript-and-redux-saga/static/js/2.f817da2a.chunk.js"
+  },
+  {
+    "revision": "8e9310fc64c905d7b948effedb1e117d",
+    "url": "/typescript-and-redux-saga/index.html"
+  }
+];
